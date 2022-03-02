7279c27c74418e31bf79326e92af98d0a9f64901
Scot Junkin <sjunkin@nvidia.com>
2022-03-02 08:51:07 -0800

    async await on tests

--- modules/audiocodes.js
+++ modules/audiocodes.js
@@ -5,30 +5,46 @@
  * started, hypothesis, recognition, end, error
  */
 
+const ASRPipe = require('../riva_client/asr');
 
-function audioCodesControlMessage(data, asr, send) {
-    msg_data = JSON.parse(data);
-    if (msg_data.type === "start") {
-        asr.setupASR(msg_data);
-        asr.mainASR(function (result) {
+function rivaTranscriptionReceived(result) {
             if (result.transcript == undefined) {
-                send(JSON.stringify({ "type": "started" }));
-                return;
+                ws.send(JSON.stringify({ "type": "started" }));
             }
             // Log the transcript to console, overwriting non-final results
             process.stdout.write(''.padEnd(process.stdout.columns, ' ') + '\r')
             if (!result.is_final) {
                 process.stdout.write('TRANSCRIPT: ' + result.transcript + '\r');
-                send(JSON.stringify({ "type": "hypothesis", "alternatives": [{ "text": result.transcript }] }));
+                ws.send(JSON.stringify({ "type": "hypothesis", "alternatives": [{ "text": result.transcript }] }));
             } else {
                 process.stdout.write('TRANSCRIPT: ' + result.transcript + '\n');
-                send(JSON.stringify({ "type": "recognition", "alternatives": [{ "text": result.transcript }] }));
-                send(JSON.stringify({ "type": "end", "reason": "Recognition complete" }));
+                ws.send(JSON.stringify({ "type": "recognition", "alternatives": [{ "text": result.transcript }] }));
+                ws.send(JSON.stringify({ "type": "end", "reason": "Recognition complete" }));
+            }
+}
+
+async function audioCodesControlMessage(data, asr, ws) {
+    msg_data = JSON.parse(data);
+    if (msg_data.type === "start") {
+        asr.setupASR(msg_data);
+        asr.mainASR(function rivaTranscriptionReceived(result) {
+            if (result.transcript == undefined) {
+                ws.send(JSON.stringify({ "type": "started" }));
+            }
+            // Log the transcript to console, overwriting non-final results
+            process.stdout.write(''.padEnd(process.stdout.columns, ' ') + '\r')
+            if (!result.is_final) {
+                process.stdout.write('TRANSCRIPT: ' + result.transcript + '\r');
+                ws.send(JSON.stringify({ "type": "hypothesis", "alternatives": [{ "text": result.transcript }] }));
+            } else {
+                process.stdout.write('TRANSCRIPT: ' + result.transcript + '\n');
+                ws.send(JSON.stringify({ "type": "recognition", "alternatives": [{ "text": result.transcript }] }));
+                ws.send(JSON.stringify({ "type": "end", "reason": "Recognition complete" }));
             }
         });
-        send(JSON.stringify({ "type": "started" }))    ;
+        ws.send(JSON.stringify({ "type": "started" }))    ;
     } else if (msg_data.type === 'stop') {
-        send(JSON.stringify({ "type": "end", "reason": "ASR service stopped" }));
+        ws.send(JSON.stringify({ "type": "end", "reason": "ASR service stopped" }));
     }
 }
 
@@ -42,8 +58,7 @@ function wsServerConnection(ws, req) {
     let asr = new ASRPipe();
     ws.on('message', function message(data, isBinary) {
         if (!isBinary) {  // non-binary data will be string start/stop control messages
-            audioCodesControlMessage(data, asr, ws.send);
-
+            audioCodesControlMessage(data, asr, ws);
         } else {
             asr.recognizeStream.write({ audio_content: data });
         }
@@ -58,4 +73,4 @@ function wsServerClose(reason) {
     console.log('closing connection %s', reason);
 };
 
-module.exports =  audioCodesControlMessage;//, wsServerConnection, wsServerClose };
+module.exports = { audioCodesControlMessage, wsServerConnection, wsServerClose } ;
